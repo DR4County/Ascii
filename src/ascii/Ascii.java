370e1147bcd40907850ab87ecebf03ff8b7ca1bd
David Reyes <20505@my4county.net>
2021-08-25 10:36:53 -0400

    keyboard

--- src/ascii/Ascii.java
+++ src/ascii/Ascii.java
@@ -15,28 +15,19 @@ public class Ascii {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("  .-\"-.");
-        System.out.println(" /|6 6|\\");
-        System.out.println(" {/(_0_)\\}");
-        System.out.println(" _/ ^ \\_");
-        System.out.println(" (/ /^\\ \\)-'");
-        System.out.println(" \"\"' '\"\"");
-        System.out.println("\n FIDO");
-        System.out.println("");
-        //Ascii of a cat
-        System.out.println("  /'\\__.__/'\\ ");
-        System.out.println("~| 0   ,  0  |~_");
-        System.out.println(" ~\\  ./\\.   /~/ _\\");
-        System.out.println("  / .|.  .|.\\| |");
-        System.out.println("  | |||  ||| | | ");
-        System.out.println("  \\ ,,,  ,,, / /");
-        // Ascii of a chair that the cat is sitting on
-        System.out.println("   _____________");
-        System.out.println("  |-=-=-=-=-=-=-|");
-        System.out.println("  || |       | ||");
-        System.out.println("  || |       | ||");
-        System.out.println("  '' '       ' ''");
-
+        System.out.println("                                  _    __        .. ..");
+        System.out.println("           /           \\         /\\   / \\  /  \\  \'\' \'\'");
+        System.out.println("          /            |        /__\\   \\  ||     || ||");
+        System.out.println("          |            |       /    \\ \\_/  \\__/  || ||");
+        System.out.println("          /        ____-_______________________________________________________________________________      ");
+        System.out.println("       __/___     /                                                                                    '\\ ");
+        System.out.println("     /  |||  \\   |  | Esc |    | F1 | F2 | F3 | F4 |  | F5 | F6 | F7 | F8 |  | F9 | F10 |   8/24/21   | |");
+        System.out.println("    | L |=| R |  | | ~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | + |   Backspace  | |    David    | |");
+        System.out.println("    |   |_|   |  | | Tab | Q | W | E | R | T | Y | U | I | O | P | [ | ] | { | } | \\  | |    Reyes    | |");
+        System.out.println("    |\\__|||__/|  | | Caps | A | S | D | F | G | H | J | K | L | ; | : | ' | \" | Enter | |             | | ");
+        System.out.println("    ||   |   ||  | | Shift | Z | X | C | V | B | N | M | , | . | / | ? | Shift        | |     | ^ |   | | ");
+        System.out.println("    \\_       _/  | | Ctrl | [x] | Alt |                   | Alt | [x] |  [=']  | Ctrl | | | < | ˅ | > | |");
+        System.out.println("     \\\\_____//   \\_____________________________________________________________________________________/");
     }
     
 }
