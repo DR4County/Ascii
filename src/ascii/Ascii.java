616a44d83b414cb95124c7d3617a050cd183a5fc
David Reyes <20505@my4county.net>
2021-08-25 09:03:22 -0400

    published

---
+++ src/ascii/Ascii.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ascii;
+
+/**
+ *
+ * @author dreyes
+ */
+public class Ascii {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("  .-\"-.");
+        System.out.println(" /|6 6|\\");
+        System.out.println(" {/(_0_)\\}");
+        System.out.println(" _/ ^ \\_");
+        System.out.println(" (/ /^\\ \\)-'");
+        System.out.println(" \"\"' '\"\"");
+        System.out.println("\n FIDO");
+        System.out.println("");
+        //Ascii of a cat
+        System.out.println("  /'\\__.__/'\\ ");
+        System.out.println("~| 0   ,  0  |~_");
+        System.out.println(" ~\\  ./\\.   /~/ _\\");
+        System.out.println("  / .|.  .|.\\| |");
+        System.out.println("  | |||  ||| | | ");
+        System.out.println("  \\ ,,,  ,,, / /");
+        // Ascii of a chair that the cat is sitting on
+        System.out.println("   _____________");
+        System.out.println("  |-=-=-=-=-=-=-|");
+        System.out.println("  || |       | ||");
+        System.out.println("  || |       | ||");
+        System.out.println("  '' '       ' ''");
+
+    }
+    
+}
